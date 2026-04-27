b80bb5d51457dd2b7bf499d52fe7c8a1a4e7c347
kazekura <gersonrcarvalho77@gmail.com>
2026-04-27 22:03:51 +0100

    patch(main): using new hash system.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ static void cameraControl(raylib::Camera2D &cam) {
 }
 
 int main(void) {
-	HashCollision		   *hash = HashCollision::getInstance();
+	HashCollision		   &hash = *HashCollision::getInstance();
 	std::vector<Particle *> particles;
 	particles.reserve(1000);
 	for (int i = 0; i < 1000; ++i) {
@@ -46,31 +46,28 @@ int main(void) {
 	const raylib::Vector2 initialPosition(window.GetSize() / 2.0f);
 	raylib::Camera2D	  cam(initialPosition, initialPosition);
 	window.SetTargetFPS(60);
+	std::vector<Particle *> colliders;
+	colliders.reserve(128);
 	while (!window.ShouldClose()) {
 		cameraControl(cam);
-		hash->addParticles(particles);
 		window.BeginDrawing();
 		window.ClearBackground();
 		cam.BeginMode();
+		hash.rebuild(particles);
 		for (Particle *particle : particles) {
 			Particle &p = *particle;
-			auto	  colliders = hash->getCollisions(particle);
+			colliders.clear();
+			hash.getCollisions(particle, colliders);
 			for (Particle *collider : colliders) {
 				p.collideWith(*collider, 0.5f);
 			}
 			p.update();
-			for (auto *particle2 : particles) {
-				auto &p2 = *particle2;
-				if (p != p2) {
-					p.applyNewtonianGravity(p2.getPos(), 100.0f);
-				}
-			}
-			p.draw();
+			p.draw(window, cam);
 		}
 		cam.EndMode();
 		window.DrawFPS();
 		window.EndDrawing();
-		hash->clear();
+		hash.clear();
 	}
 	for (Particle *p : particles) {
 		delete p;
